d0f76ce5f603fdd0b36d331ae2274522c4b13d45
Muhammad Hussain <muhammadhussain@muhammads-mbp.home>
2024-07-15 22:58:01 +0100

    4-get-article-by-id

--- db/app.js
+++ db/app.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const { getTopics } = require("../controllers/topics.controller");
-const apiController = require('../controllers/api.controller')
+const { getArticleById} = require('../controllers/articles.controller')
+const {getEndpoints} = require('../controllers/api.controller')
+
 
 const app = express();
 
-app.use("/api/topics", getTopics);
 
 app.use(express.json())
-app.use('/api', apiController.getEndpoints)
+app.get("/api/topics", getTopics);
 
+app.get('/api', getEndpoints)
+
+app.get('/api/articles/:article_id', getArticleById)
 
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
   } else {
-    res.status(500).send({ msg: "Internal Server Error" });
+    res.status(404).send({ msg: "Not Found" });
   }
 });
 
