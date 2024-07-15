952a2ef5dde1c327c0d4c11ec4f53d85df6e43f2
Muhammad Hussain <muhammadhussain@muhammads-mbp.home>
2024-07-15 12:20:47 +0100

    2 get topics

---
+++ __tests__/app.test.js
@@ -0,0 +1,27 @@
+const request = require("supertest");
+const app = require("../db/app");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe("GET /api/topics", () => {
+  it("should return an array of topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(Array.isArray(body.topics)).toBe(true);
+        body.topics.forEach((topic) => {
+          expect(topic).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
---
+++ db/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { getTopics } = require("../controllers/topics.controller");
+const app = express();
+
+app.use("/api/topics", getTopics);
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    res.status(500).send({ msg: "Internal Server Error" });
+  }
+});
+
+module.exports = app;
