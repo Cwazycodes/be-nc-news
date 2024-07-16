75648bc36b80305b8d484ec558e0f94564c284c2
Muhammad Hussain <muhammadhussain@muhammads-mbp.home>
2024-07-16 12:18:47 +0100

    5-get-article

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,16 +1,29 @@
 const db = require("../db/connection");
-const { fetchArticleById } = require("../models/article.models");
+const { fetchArticleById, fetchArticles } = require("../models/article.models");
 
 const getArticleById = (req, res, next) => {
   const { article_id } = req.params;
 
   fetchArticleById(article_id)
     .then((article) => {
-      res.status(200).send({ article });
+        if (!article) {
+            return Promise.reject({status: 404, msg:'Not Found'})
+        }
+      res.status(200).json({ article });
     })
     .catch((err) => {
       console.error(err);
       next(err);
     });
 };
-module.exports = { getArticleById };
+
+const getArticles = (req,res,next) => {
+    fetchArticles()
+    .then(articles => {
+        res.status(200).json({articles})
+    })
+    .catch(err => {
+        next(err)
+    })
+}
+module.exports = { getArticleById, getArticles };
--- models/article.models.js
+++ models/article.models.js
@@ -6,7 +6,7 @@ const fetchArticleById = (id) => {
     .then((result) => {
       if (result.rows.length === 0) {
         return Promise.reject({
-          message: "Article does not exist",
+          message: "Not Found",
           status: 404,
         });
       }
@@ -15,4 +15,41 @@ const fetchArticleById = (id) => {
     });
 };
 
-module.exports = { fetchArticleById };
+
+const fetchArticles = () => {
+    return db.query(`
+    SELECT
+        articles.author,
+        articles.title,
+        articles.article_id,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        COUNT(comments.comment_id) AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC;`).then((result) => {
+    
+      return result.rows.map(article =>
+        
+        (
+        
+        {
+        author: article.author,
+        title: article.title,
+        article_id: article.article_id,
+        topic: article.topic,
+        created_at: new Date(article.created_at),
+        votes: article.votes,
+        article_img_url: article.article_img_url,
+        comment_count: parseInt(article.comment_count) || 0
+      }));
+    })
+    .catch(err => {
+        throw new Error('Error fetching articles: ${err.message}')
+    })
+  };
+
+module.exports = { fetchArticleById , fetchArticles};
